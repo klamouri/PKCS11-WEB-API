dc6865b3f6b68646a895fafbf3eb973d3ffa8598
Karim Lamouri <karimlamouri@me.com>
2016-01-04 16:54:16 +0100

    Fix ModuleWS

--- src/main/java/api/webservice/implementation/ModuleWebServiceImplementation.java
+++ src/main/java/api/webservice/implementation/ModuleWebServiceImplementation.java
@@ -25,12 +25,13 @@ public class ModuleWebServiceImplementation {
 		
 		try {
 			module.initialize(null);
-			req.getSession().setAttribute("module", module);
-			return Response.status(Status.NO_CONTENT).build();
+			
 		} catch (TokenException e) {
-			throw new WebApplicationException(
-					Response.status(Status.BAD_REQUEST).entity(new ErrorEntity("Module already initialized or not initializable")).build());
+			//throw new WebApplicationException(
+			//		Response.status(Status.BAD_REQUEST).entity(new ErrorEntity("Module already initialized or not initializable")).build());
 		}
+		req.getSession().setAttribute("module", module);
+		return Response.status(Status.NO_CONTENT).build();
 		
 	}
 
@@ -41,12 +42,13 @@ public class ModuleWebServiceImplementation {
 					Response.status(Status.BAD_REQUEST).entity(new ErrorEntity("Module not initialized")).build());
 		try {
 			m.finalize();
-			req.getSession().removeAttribute("module");
-			return Response.status(Status.NO_CONTENT).build();
+			
 		} catch (Throwable e) {
-			throw new WebApplicationException(
-					Response.status(Status.BAD_REQUEST).entity(new ErrorEntity("Module can't be finalized")).build());
+			//throw new WebApplicationException(
+			//		Response.status(Status.BAD_REQUEST).entity(new ErrorEntity("Module can't be finalized")).build());
 		}
+		req.getSession().removeAttribute("module");
+		return Response.status(Status.NO_CONTENT).build();
 		
 	}
 
